2da5842eeb64e20a379d02245c7a6a2b89f9a62b
elcih17 <hicle17@gmail.com>
2017-10-30 15:31:23 +0900

    fix notify.sh

--- scripts/notify.sh
+++ scripts/notify.sh
@@ -5,7 +5,9 @@ abs_path=`echo $(cd $(dirname $0) && pwd)`
 . ${abs_path}/notify_to_slack.sh
 
 curr_build_id=$CIRCLE_BUILD_NUM #今回のビルドID
+echo curr_build_id $curr_build_id
 BUILD_RESULT_URL="https://${CIRCLE_DOMAIN}/gh/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/${curr_build_id}"
+echo build_result_url $BUILD_RESULT_URL
 
 test_fail_cnt=$(cat $BUILD_RESULT_FILE | jq '[.steps[].actions[] | select(contains({failed:true})) | .status] | length')
 echo test_fail_cnt $test_fail_cnt
@@ -14,12 +16,15 @@ SLACK_USER_NAME="okdBot"
 SLACK_CHANNEL="#okada_test"
 SLACK_WEBHOOK_URL="https://hooks.slack.com/services/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"
 
+echo notify to slack: $SLACK_USER_NAME $SLACK_CHANNEL
+
 # develop or masterブランチでのテストに失敗していたら通知する
 TARGETS=("develop" "master")
 
 if [ "$test_fail_cnt" -gt 0 ]; then
-  for branch in $TARGETS;
+  for branch in "${TARGETS[@]}";
   do
+    echo branch $branch
     if [[ "$CIRCLE_BRANCH" = "$branch"]]; then
       last_committer="okd"
       echo last_committer:$last_committer
@@ -33,7 +38,8 @@ fi
 # develop or masterブランチで前回のビルド結果がfailed、今回のビルド結果がfixedの場合に通知する
 if [ "$test_fail_cnt" -eq 0]; then
   for branch in "${TARGETS[@]}";
-    do
+  do
+    echo branch $branch
     if [[ "$CIRCLE_BRANCH" = "$branch"]] && [ "$PREVIOUS_BUILD_STATUS" = "failed" ]; then
       message="<!here>\n 前回失敗していたdevelopブランチのテストが成功しました。 :white_check_mark: \n $BUILD_RESULT_URL"
       notify_to_slack "$message" $SLACK_USER_NAME $SLACK_CHANNEL $SLACK_WEBHOOK_URL
